20e5cbf54a882061e992a7cf57eda0918b522f60
LucasMatos <lucasmatosbognotti@gmail.com>
2020-04-10 12:38:20 -0300

    successfully completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [ respositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      url: 'https://rocketseat.com.br',
+      title: `Novo repositorio ${Date.now()}`,
+      techs: ['Node, React, React-Native']
+    });
+
+    console.log(response.data);
+
+    setRepositories([...respositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`);
+    // Vai retornar e setar os valores que for diferente, o que for igual o filter descarta
+    setRepositories(respositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {respositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
